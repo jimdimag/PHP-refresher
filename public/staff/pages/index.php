98cd9201fbb1ab7ee9a60622d9950ba273cfb78f
Jim DiMaggio <jimdimag@gmail.com>
2019-10-03 12:53:24 -0400

    Added navigation functionality

--- public/staff/pages/index.php
+++ public/staff/pages/index.php
@@ -1,7 +1,9 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
 <?php
+
   $page_set = find_all_pages();
+
 ?>
 
 <?php $page_title = 'Pages'; ?>
@@ -18,7 +20,7 @@
   	<table class="list">
   	  <tr>
         <th>ID</th>
-        <th>Subject ID</th>
+        <th>Subject</th>
         <th>Position</th>
         <th>Visible</th>
   	    <th>Name</th>
@@ -28,21 +30,22 @@
   	  </tr>
 
       <?php while($page = mysqli_fetch_assoc($page_set)) { ?>
+        <?php $subject = find_subject_by_id($page['subject_id']); ?>
         <tr>
           <td><?php echo h($page['id']); ?></td>
-          <td><?php echo h($page['subject_id']); ?></td>
+          <td><?php echo h($subject['menu_name']); ?></td>
           <td><?php echo h($page['position']); ?></td>
           <td><?php echo $page['visible'] == 1 ? 'true' : 'false'; ?></td>
     	    <td><?php echo h($page['menu_name']); ?></td>
           <td><a class="action" href="<?php echo url_for('/staff/pages/show.php?id=' . h(u($page['id']))); ?>">View</a></td>
           <td><a class="action" href="<?php echo url_for('/staff/pages/edit.php?id=' . h(u($page['id']))); ?>">Edit</a></td>
-          <td><a class="action" href="<?php echo url_for('/staff/pages/delete.php?id=' . h(u($page['id'])));?> ">Delete</a></td>
+          <td><a class="action" href="<?php echo url_for('/staff/pages/delete.php?id=' . h(u($page['id']))); ?>">Delete</a></td>
     	  </tr>
       <?php } ?>
   	</table>
-<?php
-mysqli_free_result($page_set);
-?>
+
+    <?php mysqli_free_result($page_set); ?>
+
   </div>
 
 </div>
